e4197e9288f0603ea608d1fe9a08fc8714129d8a
Prince-Choubey <choubeyprince379@gmail.com>
2026-04-13 20:50:45 +0530

    Working on the events

---
+++ lib/bloc/counter/counter_bloc.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/bloc/counter/counter_event.dart';
+import 'package:flutter_bloc/bloc/counter/counter_state.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState>{
+  CounterBloc(): super(const CounterState()){
+    on<IncrementCounter>(_increment);
+  }
+
+  void _increment(IncrementCounter event, Emitter<CounterState> emit){
+    
+  }
+  
+}
