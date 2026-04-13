ba04db94da8c383e58a7afe4d7bbaab3a7bb293c
Prince-Choubey <choubeyprince379@gmail.com>
2026-04-13 20:40:57 +0530

    Working on the states

---
+++ lib/bloc/counter/counter_event.dart
@@ -0,0 +1,14 @@
+// we cannot initialize the abst
+import 'package:equatable/equatable.dart';
+
+abstract class CounterEvent extends Equatable {
+  const CounterEvent();
+  @override
+  List<Object?> get props => [];
+}
+
+class IncrementCounter extends CounterEvent{
+
+}
+
+class DecrementCounter extends CounterEvent {}
